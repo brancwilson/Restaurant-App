c59c8a3ce0fb48b7d5dc8cc6c28a53af5efba53d
brancwilson <57641200+brancwilson@users.noreply.github.com>
2025-02-11 16:23:40 -0600

    add item page

---
+++ additem.php
@@ -0,0 +1,85 @@
+<?php
+
+$db_host = "c8m0261h0c7idk.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com";
+$db_port = "5432";
+$db_name = "dpe2kq3p3j0dv";
+$db_username = "u4bum5vo1sv2r2";
+$db_password = "pe20a594001c2be5002cbb2aa26bc527b13edc6673e3e1376cd4dc6753ff89238";
+
+
+try {
+    $dsn = "pgsql:host=$db_host;port=5432;dbname=$db_name;";
+    // make a database connection
+    $pdo = new PDO($dsn, $db_username, $db_password, [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
+    
+
+    if ($pdo) {
+        if (isset($_POST["itemName"]) && isset($_POST["itemPrice"]) && isset($_POST["itemType"]) && isset($_POST["itemAvailability"])) {
+
+            $itemName = $_POST['itemName'];
+            $itemPrice = $_POST['itemPrice'];
+            $itemType = $_POST['itemType'];
+            $itemAvailability = $_POST['itemAvailability'];
+
+        }
+
+        $sql = "INSERT INTO MenuItems(itemName, itemPrice, itemType, itemAvailability) VALUES (?, ?, ?, ?)";
+        $stmt= $pdo->prepare($sql);
+        $stmt->execute([$itemName, $itemPrice, $itemType, $itemAvailability]);
+    
+    } else {
+        echo "pdo fail...";
+    }
+} catch (PDOException $e) {
+    die($e->getMessage());
+} finally {
+    if ($pdo) {
+        $pdo = null;
+    }
+}
+?>
+
+<!DOCTYPE html>
+<head>
+    <title>Form Page</title>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+</head>
+<body>
+    <h1>Add Item</h1>
+    <form method="post">
+        <table>
+            <tr>
+                <td>Item Name:</td>
+                <td>
+                    <input id="itemName" name="itemName" type="text">
+                </td>
+            </tr>
+            <tr>
+                <td>Item Price:</td>
+                <td>
+                    <input id="itemPrice" name="itemPrice" type="text">
+                </td>
+            </tr>
+            <tr>
+                <td>Item Type:</td>
+                <td>
+                    <input id="itemType" name="itemType" type="text">
+                </td>
+            </tr>
+            <tr>
+                <td>Item Availability:</td>
+                <td>
+                    <input id="itemAvailability" name="itemAvailability" type="text">
+                </td>
+            </tr>
+        </table>
+        <input id="newItemBtn" type="submit" value="Add Item">
+    </form>
+
+    <div>
+        <table id="Menu Items">
+            
+        </table>
+    </div>
+</body>
+</html>
---
+++ script/additem.js
@@ -0,0 +1,24 @@
+$("document").ready(function(){
+
+    $("#newItemBtn").click(function() {
+        console.log("Add Item Button clicked...");
+
+        event.preventDefault();
+
+        var itemName = $("#itemName").val();
+        var itemPrice = $("#itemPrice").val();
+        var itemType = $("#itemType").val();
+        var itemAvailability = $("#itemAvailability").val();
+
+        $.ajax({
+            url: 'additem.php',
+            type: 'post',
+            //data must be sent as a key value pair - {dataName: javascriptData}
+            data: {itemName: itemName, itemPrice: itemPrice, itemType: itemType, itemAvailability: itemAvailability},
+            success: function() {
+                alert("Item added");
+            }
+        });
+    })
+
+});
