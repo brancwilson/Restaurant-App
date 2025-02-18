17d210ee12c7639002adf7a45b90aa492d273203
brancwilson <brancwilson@gmail.com>
2025-02-18 16:43:53 -0600

    Update settings.php

--- pages/settings.php
+++ pages/settings.php
@@ -9,8 +9,21 @@ if (!isset($_SESSION['user'])) {
 require_once __DIR__ . '/../templates/header.php';
 ?>
 
-<h1>Settings</h1>
-<p>Welcome to the settings page. Here you can manage your account and preferences.</p>
-<a href="logout.php" class="button">Logout</a>
 
-<?php require_once __DIR__ . '/../templates/footer.php'; ?>
+<!DOCTYPE html>
+
+<head>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+    <script src="/../../public/js/main.js>"></script>
+</head>
+
+<body>
+    <h1>Settings</h1>
+    <p>Welcome to the settings page. Here you can manage your account and preferences.</p>
+
+    <a href="additem.php" class="button">Edit Menu</a>
+    <a href="logout.php" class="button">Logout</a>
+    
+    <?php require_once __DIR__ . '/../templates/footer.php'; ?>
+</body>
+</html>
