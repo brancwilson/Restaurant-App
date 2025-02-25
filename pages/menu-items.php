4efc5b4a66ec3c6aeee761fae2206000bbd634ef
gmco <gmco.magnet@gmail.com>
2025-02-25 15:18:19 -0600

    Update menu-items.php

--- pages/menu-items.php
+++ pages/menu-items.php
@@ -1,6 +1,6 @@
 <?php
 require_once __DIR__ . '/../includes/functions.php';
-require_once __DIR__ . '/../phpfunctions/getMenuItems';
+require_once __DIR__ . './phpfunctions/getMenuItems';
 session_start();
 
 if (!isset($_SESSION['user'])) {
