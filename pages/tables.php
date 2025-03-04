17b05d43255fac0c2232bfa31833bbc0b79b7383
gmco <gmco.magnet@gmail.com>
2025-03-04 14:43:12 -0600

    Update tables.php

--- pages/tables.php
+++ pages/tables.php
@@ -23,6 +23,10 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
 require_once __DIR__ . '/../templates/header.php';
 ?>
+<h1><?php var_dump($numTables) ?></h1>
+<h1><?php var_dump($numTables['optionvalue']) ?></h1>
+
+<h1>Select a Table</h1>
 <h1><?php echo $numTables['optionvalue'] ?></h1>
 <h1>Select a Table</h1>
 <div class="table-layout">
