4d857a6b97850d8ecb96cf01ec122bf276b1d2c2
gmco <gmco.magnet@gmail.com>
2025-03-05 12:28:42 -0600

    Update header.php

--- templates/header.php
+++ templates/header.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>POS System</title>
     <link rel="stylesheet" href="/public/css/style.css">
-    <script src="/../public/js/additem.js"
+    <script src="/../public/js/additem.js"></script>
     <!-- Add FontAwesome for the settings icon -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
 </head>
