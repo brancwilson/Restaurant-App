1ca9e12e8df1b9ec9bbd9aa2349a07d24cd804cb
gmco <gmco.magnet@gmail.com>
2025-03-13 15:45:05 -0500

    Update checkout.php

--- pages/checkout.php
+++ pages/checkout.php
@@ -39,8 +39,8 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     // Mark the table as busy
     $_SESSION['tables'][$table] = 'busy';
     
-    createTableOrder($table[0], compileOrderItemIDs($selectedItems), $orderId);
-    setTableStatus($table[0], 'busy');
+    createTableOrder($table, compileOrderItemIDs($selectedItems), $orderId);
+    setTableStatus($table, 'busy');
     updateTableSession();
 
 
