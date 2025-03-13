2bc2ef85983fb8c01c93e2fc901d65ba5715d531
gmco <gmco.magnet@gmail.com>
2025-03-13 14:38:49 -0500

    test

--- pages/checkout.php
+++ pages/checkout.php
@@ -20,6 +20,7 @@ $selectedItems = $_SESSION['cart'][$table];
 $total = calculateTotal($selectedItems);
 
 $test = compileOrderItemIDs($selectedItems);
+var_dump($test);
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     if (!isset($_SESSION['submitted_orders'])) {
