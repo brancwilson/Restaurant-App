10ae3cb40e638e8be0fea6a413fa0b59e87a50f7
gmco <gmco.magnet@gmail.com>
2025-03-25 14:48:10 -0500

    update

--- pages/options.php
+++ pages/options.php
@@ -8,6 +8,9 @@
         <td><input type="number" id="numTables"></input></td>
         <td><button id="numTablesSubmitBtn">Submit</button></td>
     </form>
+    <br>
+    <a href="/../resetOrders.php" class="button">Reset All Orders and Order Items</a>
+
 <tr>
 </table>
 
