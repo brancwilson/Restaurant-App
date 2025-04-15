e265c31465cc539e7fc202f6df6301f3d0118892
d4ruku <dio05032003@gmail.com>
2025-04-15 16:12:05 -0500

    1

--- pages/checkout.php
+++ pages/checkout.php
@@ -36,7 +36,7 @@ $stmt = $conn->prepare($sql);
 $stmt->execute([':table_id' => $table]);
 $tableStatus = $stmt->fetchColumn();
 
-if ($tableStatus !== 'pending') {
+if ($tableStatus !== 'open') {
     die("The table is not open. Please select a different table.");
 }
 
