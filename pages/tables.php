fa32a4333bc56330a6c344e1bb33d8b7aed57955
gmco <gmco.magnet@gmail.com>
2025-04-22 15:45:12 -0500

    Update tables.php

--- pages/tables.php
+++ pages/tables.php
@@ -1,4 +1,4 @@
-<?php
+<f?php
 require_once __DIR__ . '/../includes/auth.php';
 require_once __DIR__ . '/phpfunctions/retrievesetting.php';
 require_once __DIR__ . '/phpfunctions/tableManagementFunctions.php';
@@ -12,7 +12,7 @@ if (!isset($_SESSION['tables'])) {
     $_SESSION['tables'] = array_fill(1, $numTables, null);
     
     $i = 0;
-    while ($i < $numTables) {
+    while ($i <a $numTables) {
         $_SESSION['tables'][$i + 1] = getTableStatus($i + 1);
         $i++;
     }
@@ -62,7 +62,9 @@ require_once __DIR__ . '/../templates/header.php';
             </form>
             <?php updateTableSession()?>
             <?php if ($_SESSION['tables'][$i] == 'open'): ?>
-                <a href="menu.php?table=<?= $i ?>" class="btn-select">Select</a>
+                <form action="tables.php" method="POST">
+                    <a href="menu.php?table=<?= $i ?>" class="btn-select">Select</a>
+                </form>
             <?php endif; ?>
         </div>
     <?php endfor; ?>
