86cd27c0421f1f490f81f11d2091e3f1e9db1eb0
gmco <gmco.magnet@gmail.com>
2025-04-22 16:46:25 -0500

    Update tableStatusValidation.php

--- pages/phpfunctions/tableStatusValidation.php
+++ pages/phpfunctions/tableStatusValidation.php
@@ -7,10 +7,11 @@ if (isset($_POST["tableNum"])) {
     $tableStatus = getTableStatus($tableNum);
 
     if ($tableStatus == 'open') {
-        echo "<script>location.href('menu.php?table=" . $tableNum . "'</script>";
+        echo "<script>location.href('menu.php?table=" . $tableNum . "';</script>";
     } else {
-        echo "<script>alert('This table has already been claimed!</script>";
-        echo "<script>location.reload()</script>";
+        echo "<script>console.log('Claimed');</script>";
+        echo "<script>location.href('menu.php?table=" . $tableNum . "';</script>";
+        //echo "<script>location.reload()</script>";
     }
 
 }
