51dcb24fccc4c5c3ba9da19df62e1aa7aa71ef34
gmco <gmco.magnet@gmail.com>
2025-04-23 18:07:46 -0500

    Update tableBusyResponse.php

--- pages/tableBusyResponse.php
+++ pages/tableBusyResponse.php
@@ -10,6 +10,6 @@ require_once __DIR__ . '/../templates/header.php';
 <div class="busyTableDiv">
     <h1>This table has become busy, sorry.</h1>
     <br>
-    <a id="returnToTablesBtn">Return to Tables</a>
+    <a href="" id="returnToTablesBtn">Return to Tables</a>
 </div>
 <?php require_once __DIR__ . '/../templates/footer.php'; ?>
