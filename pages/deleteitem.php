d759ddfd3141d1e13f34032a780393a716d5899b
gmco <gmco.magnet@gmail.com>
2025-04-24 16:03:05 -0500

    Update deleteitem.php

--- pages/deleteitem.php
+++ pages/deleteitem.php
@@ -14,15 +14,11 @@ try {
     
 
     if ($pdo) {
-        if (isset($_POST["numTables"])) {
-            
-            if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST["deleteItem"])) {
+        if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST["deleteItem"])) {
                 $toDelete = $_POST["deleteItem"];
                 $sql = "DELETE FROM menuitems WHERE itemid = ?";
                 $stmt = $pdo->prepare($sql);
                 $stmt->execute([$toDelete]);
-            }
-
         }
     
     } else {
